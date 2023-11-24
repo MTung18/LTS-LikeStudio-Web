4a4961a74b3289d5979234caa6e174ad0ac6fc37
anhnguyen <nguyennhoanganh009@gmail.com>
2023-11-24 15:20:17 +0700

    done contact 11 for admin page

--- npm-vue3-kt-like-studio-service/src/views/customer-service/inquiries/create/index.vue
+++ npm-vue3-kt-like-studio-service/src/views/customer-service/inquiries/create/index.vue
@@ -1,78 +1,38 @@
 <template>
+  <div class="text-5xl" @click="c">check</div>
   <TemplateEdit title="1:1 문의하기">
     <template #body>
       <TemplateEditTextFields label="카테고리" required>
-        <DropdownSelect :select-list="dummySelect" :default-select="'선택'" />
+        <DropdownSelect :select-list="lsSupportManagerCategoryListData" :default-select="'login'"
+          @select="setCurrentCategory" />
       </TemplateEditTextFields>
-      <TemplateEditTextFields
-        :fields-id="inputId"
-        label="제목"
-        required
-        v-model="inputRef"
-      >
-        <TextFields
-          v-bind="$attrs"
-          :input-id="inputId"
-          v-model="inputRef"
-          size="medium"
-          placeholder="제목 입력"
-          class-bind="w-full"
-        />
+      <TemplateEditTextFields :fields-id="titleId" label="제목" required v-model="title">
+        <TextFields v-bind="$attrs" :input-id="titleId" v-model="title" size="medium" placeholder="제목 입력"
+          class-bind="w-full" />
       </TemplateEditTextFields>
-      <TemplateEditTextFields
-        :fields-id="inputId"
-        label="내용"
-        required
-        v-model="inputRef"
-        class="!pb-[3.2rem]"
-      >
-        <TextArea
-          v-bind="$attrs"
-          :textarea-id="textareaId"
-          v-model="textareaRef"
-          placeholder="내용 입력"
-          class-bind="w-full"
-        />
+      <TemplateEditTextFields :fields-id="contentIdId" label="내용" required class="!pb-[3.2rem]">
+        <TextArea v-bind="$attrs" :textarea-id="textareaId" v-model="content" placeholder="내용 입력" class-bind="w-full" />
       </TemplateEditTextFields>
 
-      <TemplateEditTextFields
-        label="첨부파일"
-        class-bind="pt-[3.2rem] !pb-[3.2rem] border-t-[1px] border-t-gray-gray-ddd"
-      >
-        <TemplateEditFileFields
-          @file-upload="handleFileUpload"
-          @file-remove="handleFileRemove"
-          :files="dummyfiles"
-          file-caption-title="첨부파일은 최대 3개, 1개 파일당 50MB 이하의 아래 확장자만 업로드 가능합니다."
-          fileMaxLength="3"
-          :file-format="[
+      <TemplateEditTextFields label="첨부파일" class-bind="pt-[3.2rem] !pb-[3.2rem] border-t-[1px] border-t-gray-gray-ddd">
+        <TemplateEditFileFields @file-upload="handleFileUpload" @file-remove="handleFileRemove" :files="showFiles"
+          file-caption-title="첨부파일은 최대 3개, 1개 파일당 50MB 이하의 아래 확장자만 업로드 가능합니다." fileMaxLength="3" :file-format="[
             '압축 파일 : zip, 7z, alz, egg',
             '문서 파일 : xls, xlsx, ppt, pptx, doc, docx, pdf',
             '이미지 파일 : jpg , jpeg , png , gif',
             '영상 파일 : mp4, wmv, asf , flv, mov, mpeg',
-          ]"
-        />
+          ]" />
       </TemplateEditTextFields>
     </template>
 
     <template #foot>
       <div class="flex mt-[6rem] justify-center gap-x-[1rem]">
-        <UIButton
-          component="button"
-          color-type="outlined"
-          size="large"
-          class-bind="!min-w-[14rem]"
-          @click="handleCreateCancel"
-          >취소</UIButton
-        >
-        <UIButton
-          component="button"
-          color-type="primary"
-          size="large"
-          class-bind="!min-w-[14rem]"
-          @click="handleCreateSubmit"
-          >등록</UIButton
-        >
+        <RouterLink to="/customer-service/inquiries"> 
+        <UIButton component="button" color-type="outlined" size="large" class-bind="!min-w-[14rem]"
+          @click="handleCreateCancel">취소</UIButton>
+</RouterLink>
+        <UIButton component="button" color-type="primary" size="large" class-bind="!min-w-[14rem]"
+          @click="handleCreateSubmit">등록</UIButton>
       </div>
     </template>
   </TemplateEdit>
@@ -80,8 +40,9 @@
 
 <script setup>
 import { v4 as uuid } from 'uuid';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
+import { useRouter } from 'vue-router';
 import DropdownSelect from '@/components/DropdownSelect/DropdownSelect.vue';
 import TemplateEdit from '@/components/TemplateEdit/TemplateEdit.vue';
 import TemplateEditFileFields from '@/components/TemplateEditFileFields/TemplateEditFileFields.vue';
@@ -90,63 +51,116 @@ import TextArea from '@/components/TextArea/TextArea.vue';
 import TextFields from '@/components/TextFields/TextFields.vue';
 import UIButton from '@/components/UIButton/UIButton.vue';
 import customToast from '@/untils/custom_toast';
+import { storeToRefs } from 'pinia';
+import { lsSupportManagerStore } from '@/stores/lsSupportManagerStore';
+import { fileManagerStore } from '@/stores/fileManagerStore';
 
-const inputId = uuid();
-const inputRef = ref('');
+const router = useRouter();
+const { addRes, lsSupportManagerCategoryList } = storeToRefs(lsSupportManagerStore());
+const { responseUploadFile } = storeToRefs(fileManagerStore());
+const lsSupportManagerCategoryListData = ref([])
+const titleId = uuid();
+const title = ref('');
+const contentId = uuid();
+const content = ref('');
 const textareaId = uuid();
 const textareaRef = ref('');
+const firstSelect = ref('');
+const files = ref([])
+const showFiles = ref([])
+const currentCategory = ref('');
+const loginUserId = 1
 
-const dummyfiles = [
-  {
-    id: 0,
-    filename: 'screenshot_5907111102.png',
-  },
-  {
-    id: 0,
-    filename: 'screenshot_02352786929249.png',
-  },
-];
 
-const dummySelect = [
-  {
-    id: 0,
-    listName: '로그인',
-  },
-  {
-    id: 1,
-    listName: '사용법',
-  },
-  {
-    id: 2,
-    listName: '저작권',
-  },
-  {
-    id: 3,
-    listName: '팀룸',
-  },
-  {
-    id: 4,
-    listName: '서비스 오류',
-  },
-];
+function c() {
+  console.log('title', title.value);
+  console.log('content', content.value);
+}
 
+onMounted(async () => {
+  await lsSupportManagerStore().getLsSupportManagerCategoryList()
+  lsSupportManagerCategoryListData.value = lsSupportManagerCategoryList.value
+
+  firstSelect.value = lsSupportManagerCategoryListData.value[0].value
+  console.log(lsSupportManagerCategoryListData.value[0].value);
+})
+async function uploadFiles() {
+  if (files.value != '') {
+    let uploadFileParam = new FormData();
+
+    files.value.forEach(e => uploadFileParam.append('files', e))
+    await fileManagerStore().uploadFile(uploadFileParam)
+
+    responseUploadFile.value.data.forEach((e, i) => showFiles.value[i].uniqFileName = e.uniqFileName)
+  }
+}
+function setCurrentCategory(param) {
+  console.log('param:', param);
+  currentCategory.value = param.id
+}
 const handleFileUpload = async (file) => {
-  await console.log('file upload', file);
+  showFiles.value.push({ oriFileName: file.name, uniqFileName: '', createUser: loginUserId })
+  files.value.push(file)
 };
 
 const handleFileRemove = async (file) => {
-  await console.log('file remove', file);
+  showFiles.value.splice(file, 1)
+  files.value.splice(file, 1)
 };
 
 const handleCreateCancel = () => {
   console.log('취소');
 };
 
-const handleCreateSubmit = () => {
-  console.log('등록');
+async function handleCreateSubmit() {
+  await uploadFiles()
+  let param = {
+    lsSupportManager: {
+      title: title.value,
+      category: currentCategory.value,
+      content: content.value,
+      createUser: loginUserId
+    },
+    fileManagerList: showFiles.value
+  }
+  await lsSupportManagerStore().add(param)
+  console.log('add res: ', addRes.value);
   customToast.success('글을 등록했습니다.');
-  // customToast.error('에러 메세지');
 };
+// const dummyfiles = [
+//   {
+//     id: 0,
+//     filename: 'screenshot_5907111102.png',
+//   },
+//   {
+//     id: 0,
+//     filename: 'screenshot_02352786929249.png',
+//   },
+// ];
+
+// const dummySelect = [
+//   {
+//     id: 0,
+//     listName: '로그인',
+//   },
+//   {
+//     id: 1,
+//     listName: '사용법',
+//   },
+//   {
+//     id: 2,
+//     listName: '저작권',
+//   },
+//   {
+//     id: 3,
+//     listName: '팀룸',
+//   },
+//   {
+//     id: 4,
+//     listName: '서비스 오류',
+//   },
+// ];
+
 </script>
 
 <style scoped>
--- npm-vue3-kt-like-studio-service/src/views/site-management/inquiries/detail/index.vue
+++ npm-vue3-kt-like-studio-service/src/views/site-management/inquiries/detail/index.vue
@@ -1,109 +1,62 @@
 <template>
   <TemplateDetail>
     <template #body>
-      <TemplateDetailHead
-        :title="dummyData.title"
-        :category="dummyData.category"
-        :writer="dummyData.writer"
-        :writer-position="dummyData.writerPosition"
-        :date="dummyData.date"
-      />
-      <TemplateDetailBody
-        :content="dummyData.content"
-        :files="dummyData.files"
-        :prev-post="dummyData.prevPost"
-        :next-post="dummyData.nextPost"
-        class="pb-[6rem] border-gray-gray-ddd border-b-[1px]"
-      />
-      <TemplateEditTextFields
-        v-if="props.state === 'unanswered' || state === 'edit'"
-        label="답변"
-        class-bind="relative pt-[2.8rem] after:absolute after:bottom-[-1px] after:left-0 after:content-[''] after:w-full after:h-[1px] after:bg-neutrals-black"
-      >
-        <TextArea
-          v-bind="$attrs"
-          :textarea-id="textareaId"
-          v-model="textareaRef"
-          placeholder="내용 입력"
-          class-bind="w-full mb-[2.8rem]"
-        />
-        <TemplateEditFileFields
-          @file-upload="handleFileUpload"
-          @file-remove="handleFileRemove"
-          :files="dummyfiles"
-          file-caption-title="첨부파일은 최대 10개, 1개 파일당 50MB 이하의 아래 확장자만 업로드 가능합니다."
-          fileMaxLength="10"
-          :file-format="[
+      <TemplateDetailHead :title="lsSupportManagerByIdData.title" :category="lsSupportManagerByIdData.valueCate"
+        :writer="lsSupportManagerByIdData.username" :writer-position="lsSupportManagerByIdData.roleName"
+        :date="lsSupportManagerByIdData.createDate" />
+      <TemplateDetailBody :content="lsSupportManagerByIdData.content" :files="lsSupportManagerByIdData.files"
+        :prev-post="preLsSupportManagerByIdData" :next-post="nextLsSupportManagerByIdData" @someEvent="callback"
+        class="pb-[6rem] border-gray-gray-ddd border-b-[1px]" />
+      <TemplateEditTextFields v-if="props.state === 'unanswered' || state === 'edit'" label="답변"
+        class-bind="relative pt-[2.8rem] after:absolute after:bottom-[-1px] after:left-0 after:content-[''] after:w-full after:h-[1px] after:bg-neutrals-black">
+        <TextArea v-bind="$attrs" :textarea-id="textareaId" v-model="textareaRef" placeholder="내용 입력"
+          class-bind="w-full mb-[2.8rem]" />
+        <TemplateEditFileFields @file-upload="handleFileUpload" @file-remove="handleFileRemove" :files="showFiles"
+          file-caption-title="첨부파일은 최대 10개, 1개 파일당 50MB 이하의 아래 확장자만 업로드 가능합니다." fileMaxLength="10" :file-format="[
             '압축 파일 : zip, 7z, alz, egg',
             '문서 파일 : xls, xlsx, ppt, pptx, doc, docx, pdf',
             '이미지 파일 : jpg , jpeg , png , gif',
             '영상 파일 : mp4, wmv, asf , flv, mov, mpeg',
-          ]"
-        />
+          ]" />
       </TemplateEditTextFields>
-      <DetailAnswer
-        v-if="props.state === 'answered'"
-        :content="dummyAnswer.content"
-        class="before:absolute before:bottom-[-1px] before:left-0 after:content-[''] before:w-full before:h-[1px] before:bg-neutrals-black"
-      >
-        <FileDownload :files="dummyAnswer.files" />
-        <TemplateEditInfo
-          :hits="dummyInfo.hits"
-          :last-modified-date="dummyInfo.lastModifiedDate"
-          :modifier="dummyInfo.modifier"
-          :modifier-position="dummyInfo.modifierPosition"
-          :created-date="dummyInfo.createdDate"
-          :writer="dummyInfo.writer"
-          :writer-position="dummyInfo.writerPosition"
-          class-bind="after:!hidden !pt-[4rem] !pb-0"
-        />
+      <DetailAnswer v-if="props.state === 'answered'" :content="lsSupportManagerByIdData.contentAnswer"
+        class="before:absolute before:bottom-[-1px] before:left-0 after:content-[''] before:w-full before:h-[1px] before:bg-neutrals-black">
+        <FileDownload class-bind="!mt-0" v-for="item in lsSupportManagerByIdData.fileManagerListAnswer" :key="item.id"
+          :files="[
+            { id: item.id, filename: item.oriFileName, filePath: item.uniqFileName },
+          ]" />
+        <TemplateEditInfo :hits="''" :last-modified-date="formatDate(lsSupportManagerByIdData.editAnswerDate)"
+          :modifier="lsSupportManagerByIdData.usernameAnswer"
+          :modifier-position="lsSupportManagerByIdData.departmentEditAnswer"
+          :created-date="formatDate(lsSupportManagerByIdData.dateAnswer)"
+          :writer="lsSupportManagerByIdData.answerEditUser"
+          :writer-position="lsSupportManagerByIdData.departmentNameAnswer" class-bind="after:!hidden !pt-[4rem] !pb-0" />
       </DetailAnswer>
     </template>
     <template #foot>
-      <div
-        v-if="state === 'unanswered' || state === 'edit'"
-        class="flex gap-x-[1rem] justify-center"
-      >
-        <UIButton
-          component="button"
-          color-type="outlined"
-          size="large"
-          class-bind="!min-w-[14rem]"
-          @click="onDeleteButton"
-          >취소</UIButton
-        >
-        <UIButton
-          component="button"
-          color-type="primary"
-          size="large"
-          class-bind="!min-w-[14rem]"
-          @click="onSubmitButton"
-          >답변 등록</UIButton
-        >
+      <div v-if="state === 'unanswered' || state === 'edit'" class="flex gap-x-[1rem] justify-center">
+        <RouterLink
+          :to="`/site-management/inquiries/${state == 'unanswered' ? '' : `${lsSupportManagerByIdData.id}/answered`}`">
+          <UIButton component="button" color-type="outlined" size="large" class-bind="!min-w-[14rem]">취소</UIButton>
+        </RouterLink>
+        <UIButton v-if="state == 'unanswered'" component="button" color-type="primary" size="large"
+          class-bind="!min-w-[14rem]" @click="answer">답변 등록</UIButton>
+        <UIButton v-else component="button" color-type="primary" size="large" class-bind="!min-w-[14rem]" @click="update">
+          답변 등록</UIButton>
       </div>
-      <div v-if="state === 'answered'" class="flex gap-x-[1rem]">
-        <UIButton
-          component="button"
-          color-type="standard"
-          size="large"
-          class-bind="!min-w-[14rem]"
-          >답변 삭제</UIButton
-        >
-        <UIButton
-          component="a"
-          href="/site-management/inquiries"
-          color-type="outlined"
-          size="large"
-          class-bind="ml-auto !min-w-[14rem]"
-          >목록</UIButton
-        >
-        <UIButton
-          component="button"
-          color-type="primary"
-          size="large"
-          class-bind="!min-w-[14rem]"
-          >답변 수정</UIButton
-        >
+      <div v-if="state === 'answered'" class="flex gap-x-[1rem] justify-between">
+        <UIButton component="button" color-type="standard" size="large" class-bind="!min-w-[14rem]"
+          @click="onDeleteButton">답변 삭제</UIButton>
+        <div class="space-x-5">
+
+          <RouterLink :to="`/site-management/inquiries`">
+            <UIButton component="a" href="/site-management/inquiries" color-type="outlined" size="large"
+              class-bind="ml-auto !min-w-[14rem]">목록</UIButton>
+          </RouterLink>
+          <RouterLink :to="`/site-management/inquiries/${lsSupportManagerByIdData.id}/edit`">
+            <UIButton component="button" color-type="primary" size="large" class-bind="!min-w-[14rem]">답변 수정</UIButton>
+          </RouterLink>
+        </div>
       </div>
     </template>
   </TemplateDetail>
@@ -111,8 +64,8 @@
 
 <script setup>
 import { v4 as uuid } from 'uuid';
-import { defineProps, ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { defineProps, ref, onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 
 import FileDownload from '@/components/FileDownload/FileDownload.vue';
 import TemplateDetailBody from '@/components/TemplateDetailBody/TemplateDetailBody.vue';
@@ -124,8 +77,13 @@ import TemplateEditTextFields from '@/components/TemplateEditTextFields/Template
 import TextArea from '@/components/TextArea/TextArea.vue';
 import UIButton from '@/components/UIButton/UIButton.vue';
 import DetailAnswer from '@/containers/customer-service/inquiries/DetailAnswer.vue';
+import customToast from '@/untils/custom_toast';
 
-const router = useRouter();
+import { storeToRefs } from 'pinia';
+import { lsSupportManagerStore } from '@/stores/lsSupportManagerStore';
+import { fileManagerStore } from '@/stores/fileManagerStore';
+import moment from 'moment';
+
 
 const props = defineProps({
   id: {
@@ -138,81 +96,168 @@ const props = defineProps({
   },
 });
 
+const { lsSupportManagerById, allLsSupportManager } = storeToRefs(lsSupportManagerStore());
+const { responseUploadFile } = storeToRefs(fileManagerStore());
+const lsSupportManagerByIdData = ref([])
+const allLsSupportManagerData = ref([])
+const preLsSupportManagerByIdData = ref([])
+const nextLsSupportManagerByIdData = ref([])
+const userId = 1
+const files = ref([])
+const showFiles = ref([])
+let preLsSupportManagerId, nextLsSupportManagerId
+const route = useRoute()
+const router = useRouter();
 const textareaId = uuid();
 const textareaRef = ref('');
+const loginUserId = 1
 
-const dummyData = {
-  title: '갤러리로 보내기 할 때 오류가 발생합니다',
-  category: '서비스 오류',
-  writer: '홍길동',
-  writerPosition: '전략마케팅팀',
-  date: '2023.01.30 11:32',
-  content:
-    '안녕하세요! 갤러리로 보내기 버튼을 누르면 오류가 발생합니다 첨부파일 참고 부탁드리고... 윈도우 노트북이고 크롬을 쓰고 있는데요 브라우저 문제인지 잘 모르겠네요 확인 부탁드려요',
-  files: [
-    {
-      id: 0,
-      filename: 'screenshot_5907111102.png',
+onMounted(async () => {
+  await lsSupportManagerStore().getLsSupportManagerById(route.params.id)
+  lsSupportManagerByIdData.value = lsSupportManagerById.value
+
+  await lsSupportManagerStore().getAllLsSupportManager()
+  allLsSupportManagerData.value = allLsSupportManager.value
+
+  getPreNextLsSupportManagerId()
+
+  await lsSupportManagerStore().getLsSupportManagerById(preLsSupportManagerId)
+  preLsSupportManagerByIdData.value = lsSupportManagerById.value
+
+  await lsSupportManagerStore().getLsSupportManagerById(nextLsSupportManagerId)
+  nextLsSupportManagerByIdData.value = lsSupportManagerById.value
+
+  lsSupportManagerByIdData.value.fileManagerListAnswer.forEach(e => {
+    showFiles.value.push({ oriFileName: e.oriFileName, uniqFileName: e.uniqFileName, createUser: loginUserId })
+  })
+});
+
+function getPreNextLsSupportManagerId() {
+  const ls = allLsSupportManagerData.value.filter(e => e.createUser == userId && e.questionId == 0)
+  const currentId = lsSupportManagerByIdData.value.id
+
+  for (let i = 0; i < ls.length; i++) {
+    if (i < ls.length - 1 && ls[i + 1].id == currentId) preLsSupportManagerId = ls[i].id
+    if (i > 0 && ls[i - 1].id == currentId) nextLsSupportManagerId = ls[i].id
+  }
+}
+function callback(postId) {
+  window.location.href = `/customer-service/inquiries/${postId}/${nextLsSupportManagerByIdData.value.status == 1 ? 'answered' : 'unanswered'}`;
+}
+function formatDate(str) {
+  return moment(str).format("YYYY.MM.DD HH:mm")
+}
+async function onDeleteButton() {
+  if (window.confirm("confirm to delete")) {
+    await lsSupportManagerStore().deleteLsSupportManagerForAdmin(lsSupportManagerByIdData.value.idAnswer, lsSupportManagerByIdData.value.id)
+    router.push('/site-management/inquiries')
+    customToast.success('successful delete answer id', lsSupportManagerByIdData.value.idAnswer);
+  }
+};
+
+
+async function answer() {
+  await uploadFiles()
+  let param = {
+    lsSupportManagerQuestionId: lsSupportManagerByIdData.value.id,
+    lsSupportManagerAnswer: {
+      content: textareaRef.value,
+      createUser: loginUserId
+    }, fileManagerList: showFiles.value
+  }
+
+  await lsSupportManagerStore().answer(param)
+};
+async function update() {
+  await uploadFiles()
+  let param = {
+    lsSupportManager: {
+      id: lsSupportManagerByIdData.value.idAnswer,
+      content: textareaRef.value,
+      editUser: loginUserId
     },
-    {
-      id: 1,
-      filename: 'screenshot_02352786929249.png',
-    },
-  ],
+    fileManagerList: showFiles.value
+  }
+
+  await lsSupportManagerStore().update(param)
 };
 
-const dummyAnswer = {
-  content:
-    '안녕하세요! 고객님. 먼저 불편을 끼쳐드려 정말 죄송합니다. 말씀하신 오류는 어젯밤 통신 장애로 인해 발생한 것으로 밤 10시경 조치 완료했습니다. 지금은 정상적으로 작동하오니 이점 참고해 다시 한 번 시도해 보세요. 라이크 스튜디오를 이용해 주셔서 정말 감사합니다. 오늘도 즐거운 하루 보내세요!',
-  files: [
-    {
-      id: 0,
-      filename: 'screenshot_5907111102.png',
-    },
-    {
-      id: 1,
-      filename: 'screenshot_02352786929249.png',
-    },
-  ],
-  date: '2023.01.30 11:32',
+async function uploadFiles() {
+  if (files.value != '') {
+    let uploadFileParam = new FormData();
+
+    files.value.forEach(e => uploadFileParam.append('files', e))
+    await fileManagerStore().uploadFile(uploadFileParam)
+
+    let answerListLength = lsSupportManagerByIdData.value.fileManagerListAnswer.length
+    responseUploadFile.value.data.forEach((e, i) => showFiles.value[i + answerListLength].uniqFileName = e.uniqFileName)
+  }
+}
+
+async function handleFileUpload(file) {
+  showFiles.value.push({ oriFileName: file.name, uniqFileName: '', createUser: loginUserId })
+  files.value.push(file)
 };
 
-const dummyfiles = [
-  {
-    id: 0,
-    filename: 'screenshot_5907111102.png',
-  },
-  {
-    id: 0,
-    filename: 'screenshot_02352786929249.png',
-  },
-];
-
-const dummyInfo = {
-  lastModifiedDate: '2023.02.12 15:34',
-  modifier: '박미래',
-  modifierPosition: '판매역량강화팀',
-  createdDate: '2023.02.11 10:16',
-  writer: '박미래',
-  writerPosition: '판매역량강화팀',
+async function handleFileRemove(file) {
+  showFiles.value.splice(file, 1)
+  files.value.splice(file, 1)
 };
+// const dummyData = {
+//   title: '갤러리로 보내기 할 때 오류가 발생합니다',
+//   category: '서비스 오류',
+//   writer: '홍길동',
+//   writerPosition: '전략마케팅팀',
+//   date: '2023.01.30 11:32',
+//   content:
+//     '안녕하세요! 갤러리로 보내기 버튼을 누르면 오류가 발생합니다 첨부파일 참고 부탁드리고... 윈도우 노트북이고 크롬을 쓰고 있는데요 브라우저 문제인지 잘 모르겠네요 확인 부탁드려요',
+//   files: [
+//     {
+//       id: 0,
+//       filename: 'screenshot_5907111102.png',
+//     },
+//     {
+//       id: 1,
+//       filename: 'screenshot_02352786929249.png',
+//     },
+//   ],
+// };
 
-const onDeleteButton = () => {
-  console.log('취소');
-  router.push('/site-management/inquiries');
-};
+// const dummyAnswer = {
+//   content:
+//     '안녕하세요! 고객님. 먼저 불편을 끼쳐드려 정말 죄송합니다. 말씀하신 오류는 어젯밤 통신 장애로 인해 발생한 것으로 밤 10시경 조치 완료했습니다. 지금은 정상적으로 작동하오니 이점 참고해 다시 한 번 시도해 보세요. 라이크 스튜디오를 이용해 주셔서 정말 감사합니다. 오늘도 즐거운 하루 보내세요!',
+//   files: [
+//     {
+//       id: 0,
+//       filename: 'screenshot_5907111102.png',
+//     },
+//     {
+//       id: 1,
+//       filename: 'screenshot_02352786929249.png',
+//     },
+//   ],
+//   date: '2023.01.30 11:32',
+// };
 
-const onSubmitButton = () => {
-  console.log('답변 등록');
-};
+// const dummyfiles = [
+//   {
+//     id: 0,
+//     filename: 'screenshot_5907111102.png',
+//   },
+//   {
+//     id: 0,
+//     filename: 'screenshot_02352786929249.png',
+//   },
+// ];
 
-const handleFileUpload = async (file) => {
-  await console.log('file upload', file);
-};
-
-const handleFileRemove = async (file) => {
-  await console.log('file remove', file);
-};
+// const dummyInfo = {
+//   lastModifiedDate: '2023.02.12 15:34',
+//   modifier: '박미래',
+//   modifierPosition: '판매역량강화팀',
+//   createdDate: '2023.02.11 10:16',
+//   writer: '박미래',
+//   writerPosition: '판매역량강화팀',
+// };
 </script>
 
 <style scoped></style>
